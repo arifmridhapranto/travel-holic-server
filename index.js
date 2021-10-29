5b65c1bbd26525dfc5c6f7a818cf692c49e5b40b
arifmridhapranto <mridhapranto647@gmail.com>
2021-10-30 03:47:23 +0600

    starting server side project

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
 const app = express();
-const port = 7000;
+const port = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfkri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Travel-database");
+    const plans = database.collection("plans");
+    // create a document to insert
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Pranto Mridha!");
